589a61c0ef237a651b39a9a9a15f9222a09aa77a
SOBIT <staking@outofbits.com>
2020-03-06 03:31:51 +0100

    Version 0.2.1, where bugs for testing viable nodes have been resolved, and logging has been improved.

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ import (
 )
 
 const ApplicationName string = "thor"
-const ApplicationVersion string = "0.2.1-experimental"
+const ApplicationVersion string = "0.2.1"
 
 func printUsage() {
     fmt.Printf(`Usage:
